04c5ce8b16256f801a7399a4d5eaeecd260073b4
IodineBluestone <123199169+IodineBluestone@users.noreply.github.com>
2023-02-08 13:15:37 -0600

    First commit

--- ParkWise/ContentView.swift
+++ ParkWise/ContentView.swift
@@ -7,20 +7,79 @@
 
 import SwiftUI
 
+extension Color {
+    static let navy = Color(red: 0, green: 0, blue: 128/255)
+}
+
 struct ContentView: View {
+    @State private var spot_one = false
+    @State private var spot_two = false
+    @State private var spot_three = false
+    let spotOne = CarSpot(spotOpen: false,spotNumber: 20)
+    let navy = Color(red: 0, green: 0, blue: 128/255)
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor( .white) ]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor( .white)]
+          }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+                Grid{
+                    GridRow{
+                        spotOne
+                        spotOne
+                        spotOne
+                    }
+                    GridRow{
+                        spotOne
+                        spotOne
+                        spotOne
+                    }
+                    GridRow{
+                        spotOne
+                        spotOne
+                        spotOne
+                    }
+                }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.blue)
+            .navigationTitle("ParkWise")
         }
-        .padding()
+        }
+    }
+
+struct CarSpot: View {
+    @State  var spotOpen = true
+    @State  var spotNumber = 100
+    var body: some View {
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("#\(spotNumber)")
+                }
+                
+                HStack{
+                    Text(spotOpen ?" 🟢" : " 🚗")
+                        .font(.title)
+                }
+                .padding()
+                HStack{
+                    Text(spotOpen ? "Open" : "Taken")
+                    Spacer()
+                }
+            }
+            .padding()
+            .background(.white)
+            .cornerRadius(10)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
