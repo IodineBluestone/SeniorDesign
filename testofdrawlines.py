3f2554db7d192cc86fcce7bf0228102a161aaa79
beaum28 <123205078+beaum28@users.noreply.github.com>
2023-04-04 15:22:50 -0500

    Update testofdrawlines.py

--- testofdrawlines.py
+++ testofdrawlines.py
@@ -12,7 +12,7 @@ counter = 0
 #spot_3 = ParkingSpot((788,492,1022,485,1237,555,864,558)
 def draw_lines(event, x, y, flags, param):
     
-    #Declare global variables
+     #Declare global variables
     global ix,iy,sx,sy,lx,ly, counter
 
     # if the left mouse button was clicked, record the starting
@@ -28,6 +28,18 @@ def draw_lines(event, x, y, flags, param):
             sx, sy = x, y
         ix,iy = x, y 
 
+        ## Writing X coordinate for spot click into file
+        file = open('ModelParking.txt','a')
+
+        data = str(ix)+'\n'
+        file.write(data)
+        file.close
+        ## Writing Y coordinate for spot click into file
+        file = open('ModelParking.txt','a')
+
+        data = str(iy)+'\n'
+        file.write(data)
+        file.close
         # Counter that counts how many clicks have taken place
         counter = counter + 1
         if(counter % 4 == 0) :
@@ -39,23 +51,26 @@ def draw_lines(event, x, y, flags, param):
        # ix, iy = -1, -1 # reset ix and iy
        # if flags == 33: # if alt key is pressed, create line between start and end points to create polygon
             cv.line(img, (lx, ly), (sx, sy), (0, 0, 127), 2, cv.LINE_AA)
-
-    print(ix,iy,x,y)
-    print(counter)
+             ## Writing X coordinate for spot click into file
     
-
-    
-
+          
 # read image from path and add callback
-img = cv.resize(cv.imread(r'C:\Users\beaum\OneDrive\Desktop\Capstone2_Project_1\noimages.jpeg'), (1280, 720))
+img= cv.resize(cv.imread(r'C:\Users\beaum\Desktop\Capstone2_Project_1\Images\model1.jpg',-1), (1280, 720))
 cv.namedWindow('image') 
 cv.setMouseCallback('image',draw_lines)
-
 while(1):
-    
+    temps = []
+    file = open('ModelParking.txt')
+    for line in file.readlines():
+            temps.append(float(line))
+            file.close() 
+    print(len(temps))
     cv.imshow('image',img)
     k = cv.waitKey(1) & 0xFF
     # press 'q' to exit
+    if (len(temps)==97):
+        print('All spots have been successfully mapped')
+        break
     if k == ord('q'):
         break
 cv.destroyAllWindows()
